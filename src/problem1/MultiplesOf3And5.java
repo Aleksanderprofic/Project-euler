fdb8d88c8228ed084b2b6fca1de959dc77a5aed4
Aleksanderprofic <aleprofic@gmail.com>
2019-04-18 12:00:35 +0200

    Fancy solution for problem 1

---
+++ src/problem1/MultiplesOf3And5.java
@@ -0,0 +1,40 @@
+package problem1;
+
+import java.util.concurrent.*;
+
+public class MultiplesOf3And5 {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+        Callable<Integer> sumMultiplesOf3 = () -> {
+            System.out.println("In 3");
+            int sum = 0;
+            for (int i = 3; i < 1000; i += 3) {
+                sum += i;
+            }
+
+            return sum;
+        };
+
+        Callable<Integer> sumMultiplesOf5 = () -> {
+            System.out.println("In 5");
+            int sum = 0;
+            for(int i = 5; i < 1000; i += 5) {
+                if(i % 3 != 0) sum += i;
+            }
+
+            return sum;
+        };
+
+        Future<Integer> future3 = executorService.submit(sumMultiplesOf3);
+        Future<Integer> future5 = executorService.submit(sumMultiplesOf5);
+
+        int wholeSum = future3.get() + future5.get();
+
+        System.out.println(future3.get() + " + " + future5.get() + " = " + wholeSum);
+
+
+        executorService.shutdown();
+    }
+}
